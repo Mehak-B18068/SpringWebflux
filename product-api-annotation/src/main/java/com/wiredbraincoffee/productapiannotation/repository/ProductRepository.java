43525864d3297921906e5d47cc5ef324196e53bf
Mehak-B18068 <b18068@students.iitmandi.ac.in>
2021-07-16 09:29:29 +0530

    code added

---
+++ product-api-annotation/src/main/java/com/wiredbraincoffee/productapiannotation/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.wiredbraincoffee.productapiannotation.repository;
+
+import com.wiredbraincoffee.productapiannotation.model.Product;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface ProductRepository extends ReactiveMongoRepository<Product,String> {
+
+}
